2dc8dcd0d4b2b683fe2f5dce2ff01a908f662094
chigozie8 <kennethchigozieokoronkwo@gmail.com>
2026-03-09 10:37:49 +0100

    Update Hero.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -50,9 +50,9 @@ export default function Hero() {
               className="text-ss ss:text-sm md:text-xs text-gray-700 font-medium mb-4"
             >
               Dive into comprehensive course outlines, get instant access to learning materials, previous exam questions, and handouts tailored for medical students. <br className="hidden ss:block" /> Track
-              your academic progress with our GPA calculator, stay informed with
+              your academic progress with our GPA calculator, stay up to date with
               the latest news and articles, <br className="hidden ss:block" />{" "}
-              and join a thriving community of fellow students and learners.
+              and become part of an active, supportive community of fellow medical learners.
             </motion.p>
             <motion.div
               variants={fadeInVariants3}
